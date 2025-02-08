c8c87c9c1908605fe5de781e619011b52ccad649
Optoed <origin-optoed@mail.ru>
2025-02-08 17:12:28 +0400

    0.10.3: CORS for containerized and local frontend

--- frontend/docker-monitoring-frontend/src/api/api.ts
+++ frontend/docker-monitoring-frontend/src/api/api.ts
@@ -1,18 +1,13 @@
 import axios from 'axios';
 
-// Для теста на локальном компьютере:
-// const API_URL = 'http://localhost:8080/containers';
-
-const API_URL = process.env.REACT_APP_BACKEND_URL;
-const FULL_ROUTE_URL = API_URL + "/containers"
-//const FULL_ROUTE_URL = 'http://backend:8080/containers';
+const API_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080';
+const BACKEND_URL = API_URL + "/containers"
 
 export const getPingResults = async () => {
-    if (!API_URL) {
-        throw new Error("Backend URL is not defined in the environment variables.");
-    }
-    console.log("API_URL = ", API_URL)
-    console.log("FULL_ROUTE_URL = ", FULL_ROUTE_URL)
-    const response = await axios.get(FULL_ROUTE_URL);
+    // if (!API_URL) {
+    //     throw new Error("Backend URL is not defined in the environment variables.");
+    // }
+    console.log("FULL_ROUTE_URL = ", BACKEND_URL)
+    const response = await axios.get(BACKEND_URL);
     return response.data;
 };
